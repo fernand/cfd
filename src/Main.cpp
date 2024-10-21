9eda82eb61f3a4818f4bbfa7677f18827b0878b6
Fernand Pajot <fernand@epigram.me>
2024-10-21 11:41:16 -0600

    .

---
+++ src/Main.cpp
@@ -0,0 +1,11 @@
+#define NOMINMAX
+
+#include <glad/glad.h>
+#define GLFW_EXPOSE_NATIVE_WIN32
+#include <GLFW/glfw3.h>
+#include <GLFW/glfw3native.h>
+#pragma comment(lib, "glfw3.lib")
+
+int main()
+{
+}
